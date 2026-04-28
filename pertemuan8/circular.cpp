a8dba6e2c1f5ac9d2c0059bc83349a123204d2cd
ealnt <fakhrianelanta10.3@gmail.com>
2026-04-28 11:49:48 +0700

    Implement circular linked list with insert and traversal

--- pertemuan8/circular.cpp
+++ pertemuan8/circular.cpp
@@ -1,1 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+struct Node{
+    int data;
+    Node* next;
+};
+
+Node* insert(Node* node, int val){
+    Node* newNode = new Node();
+    newNode->data = val;
+    newNode->next = NULL;
+
+    if(node == NULL){
+        newNode->next = newNode;
+        return newNode;
+    }
+
+    Node* temp = node;
+    while(temp->next != node){
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    newNode->next = node;
+
+    return node;
+}
+
+void traversal(Node* node){
+    if(node == NULL) return;
+
+    Node* temp = node;
+    do{
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    } while(temp != node);
+
+    cout << "(Kembali ke head)\n";
+}
+
+int main(){
+    Node* node = NULL;
+
+    int n;
+    cout << "Banyak data: ";
+    cin >> n;
+
+    while(n--){
+        int x;
+        cout << "Masukkan data: ";
+        cin >> x;
+
+        node = insert(node, x);
+    }
+
+    cout << "Visualisasi:\n";
+    traversal(node);
+
+    return 0;
+}
