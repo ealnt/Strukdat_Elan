054ea5b1d6edaec52fb42dcbaa5dcf1cd096dc04
ealnt <fakhrianelanta10.3@gmail.com>
2026-04-28 11:47:41 +0700

    Implement singly linked list with insertion and traversal

--- pertemuan8/singular.cpp
+++ pertemuan8/singular.cpp
@@ -1,1 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+struct Node{
+    int data;
+    Node* next;
+};
+
+Node* insert(Node* node, int val){
+    Node* newNode = new Node();
+    newNode->data = val;
+    newNode->next = NULL;
+
+    if(node == NULL){
+        return newNode;
+    }
+
+    Node* temp = node;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+
+    return node;
+}
+
+void traversal(Node* node){
+    Node* temp = node;
+    while(temp != NULL){
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    
+    cout << "NULL\n";
+}
+
+int main(){
+    Node* node = NULL;
+
+    int n;
+    cout << "Banyak data: ";
+    cin >> n;
+
+    while(n--){
+        int x;
+        cout << "Masukkan data: ";
+        cin >> x;
+
+        node = insert(node, x);
+    }
+
+    cout << "Visualisasi:\n";
+    traversal(node);
+
+    return 0;
+}
